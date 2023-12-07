510c0c3a45778c5094e12edae7a7201b187a45e0
Daniele Silvestro <silvestro.daniele@gmail.com>
2023-12-07 22:26:19 +0100

    get_features_names

--- deepdive/feature_extraction.py
+++ deepdive/feature_extraction.py
@@ -152,3 +152,12 @@ def normalize_labels(Yt, rescaler=0, log=False):
     return Yt_r
 
 
+def get_features_names(n_areas):
+    global_features = ["n_species", "n_occs", "n_singletons", "n_endemics",
+                       "time_bin_duration", "range_through_div", "n_localities"]
+
+    sp_area = ["n_species_area_%s" % i for i in range(1, 1 + n_areas)]
+    occs_area = ["n_occs_area_%s" % i for i in range(1, 1 + n_areas)]
+    loc_area = ["n_locs_area_%s" % i for i in range(1, 1 + n_areas)]
+
+    return(global_features + sp_area + occs_area + loc_area)
