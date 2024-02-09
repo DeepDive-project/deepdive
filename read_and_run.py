9a1bbfee1b6851b25dfd647e7074f202538226f3
COOPER Rebecca <rebecca.cooper@unifr.ch>
2024-02-09 14:41:45 +0100

    model_training

--- read_and_run.py
+++ read_and_run.py
@@ -12,8 +12,9 @@ config = configparser.ConfigParser()
 
 # wd = "/Users/dsilvestro/Software/DeepDive-project/deepdive/test_deepdiveR/"
 wd = "/Users/CooperR/Documents/GitHub/deep_dive/"
-config_f = "try1.ini"
-config.read(os.path.join(wd, config_f))
+data_wd = "/Users/CooperR/Documents/GitHub/DeepDiveR/R/test_empirical_data/carnivora_analysis"
+config_f = "try2.ini"
+config.read(os.path.join(data_wd, config_f))
 config.sections()  # see which blocks are listed in the config
 # "simulations" in config  # to see if a block is present in the config, returns True/False
 # config["simulations"]["eta"]  # to call settings
@@ -26,24 +27,29 @@ bd_sim, fossil_sim = dd.create_sim_obj_from_config(config)
 sp_x = bd_sim.run_simulation(print_res=True)
 sim = fossil_sim.run_simulation(sp_x)
 
+# edit a setting in python after the fact
+config["simulations"]["n_training_simulations"] = '20'
+config["simulations"]["n_test_simulations"] = '20'
+
 # Run simulations in parallel
-if config.getint("simulations", "n_training_simulations"):
+if "simulations" in config.sections(): ##### should we just only have the sims + test batch option, and other test batches use independently the training module??
     feature_file, label_file = dd.run_sim_from_config(config)
 else:
-    feature_file, label_file = None
-# Train a model
-if config.getint("model_training", ""):
-    dd.run_model_training_from_config(config, feature_file=feature_file, label_file=label_file)
+    feature_file = None
+    label_file = None
+if "simulations" in config.sections() and config.getint("simulations", "n_test_simulations"):  ## check this part functions
+    test_feature_file, test_label_file = dd.run_test_sim_from_config(config)
+else:
+    test_feature_file = None
+    test_label_file = None
 # Simulate test set - just simulating, second test module that runs with models. Or maybe setting up training and test set
 # don't need to be seperate? Add a default of automatically the same settings at the training set?? It would just change
 # seed and number of simulations.
-if config.getint("", ):  #### IF x2, sim training and if test under same conditions as simulate then read settings from simulate.
-    dd.run_test_sim_from_config()
-
-
-
-
-# Make diversity predictions
+# Train a model
+if "model_training" in config.sections():
+    dd.run_model_training_from_config(config, feature_file=feature_file, label_file=label_file)
+# Predict diversity curves
+if "empirical_predictions" in config.sections():
 
 
 
@@ -64,107 +70,9 @@ running the full thing and checking each block still functions independently, ne
 How will it be run in practice? 
 
 6. empirical example
+carnivorans in the Cenozoic, global records from Faurby et al. Dispersal ability predicts volutionary sucess amond
+mammalian carnivores.
 
 7. documentation and writing the application note
 
 """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for key in config["simulations"]:  # lists settings included in a block
-    print(key)
-
-    try:
-        os.mkdir('simulations/')
-    except:
-        pass
-    try:
-        os.mkdir('simulations/sqs_' + now)
-    except:
-        pass
-
-# from the start of the model training script
-wd = "./simulations"
-model_wd = "./model_training"
-
-try:
-    os.mkdir('model_training/')
-except:
-    pass
-
-
-if __name__ == "__main__":
-    ### SIMULATE MULTIPLE DATASETS ###
-    # parallel simulations
-
-    training_seed = config.getint("simulations", "training_seed")
-    test_seed = config.getint("simulations", "test_seed")
-    if training_seed == test_seed:
-        print("Training and test seed are the same, please adjust one of them")
-
-    if config.getint("simulations", "n_training_simulations"):
-        if config.getint("simulations", "n_CPUS") > 1:
-            list_args = list(np.arange(config.getint("simulations", "n_CPUS")))
-            print("\nSimulating training data...")
-            pool = multiprocessing.Pool()
-            res = pool.map(run_sim, list_args, config=config)
-            pool.close()
-
-            features = []
-            labels = []
-
-            for i in range(config.getint("simulations", "n_CPUS")):
-                features = features + res[i]['features']
-                labels = labels + res[i]['labels']
-
-        else:
-            res = run_sim(0, config=config)
-            features = res["features"]
-            labels = res["labels"]
-
-        Xt = np.array(features)
-        Yt = np.array(labels)
-        print(Xt.shape, Yt.shape)
-        # save simulations
-        out_path = config["simulations"]["out_path"]
-        out_name = config["simulations"]["out_name"]
-        np.save(os.path.join(out_path, out_name + "_features" + now + ".npy"), Xt)
-        np.save(os.path.join(out_path, out_name + "_labels" + now + ".npy"), Yt)
-
-        print("Training features saved as: \n", os.path.join(out_path, out_name + "_features" + now + ".npy"))
-        print("Training labels saved as: \n", os.path.join(out_path, out_name + "_labels" + now + ".npy"))
-
-        ### TEST DATASETS ###
-    if config.getint("simulations", "n_test_simulations"):
-        res = run_test_sim(0, config=config)
-
-        features = res['features']
-        labels = res['labels']
-
-        Xt = np.array(features)
-        Yt = np.array(labels)
-        print(Xt.shape, Yt.shape)
-        # save simulations
-        np.save(os.path.join(out_path, out_name + "test_features" + now + ".npy"), Xt)
-        np.save(os.path.join(out_path, out_name + "test_labels" + now + ".npy"), Yt)
-
-        print("Test features saved as: \n", os.path.join(out_path, out_name + "test_features" + now + ".npy"))
-        print("Test labels saved as: \n", os.path.join(out_path, out_name + "test_labels" + now + ".npy"))
-        sim_settings = res['settings']
-        dd.save_pkl(sim_settings, os.path.join(out_path, "test_sim_settings" + now + ".pkl"))
-
-    print("\ndone.\n")
